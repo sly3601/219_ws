5496c21e5a644a98486136834139ea73a53dc46b
sly3601 <1967584065@qq.com>
2026-04-30 19:35:20 +0800

    23

--- src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/FSM/StateTrotting.cpp
+++ src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/FSM/StateTrotting.cpp
@@ -614,10 +614,10 @@ void StateTrotting::calcTau() {
 
         // ========== 新增：MIT模式下，这里更适合作为前馈/偏置力矩，而不是直接满量目标力矩 ==========
         // 足底反力没有精确控制，而是变成偏置力矩*小于1的比例系数进行修正控制
-        const double tau_ff_scale = 0.35;   // 衰减系数 先从0.25开始，后面可再调大
+        const double tau_ff_scale = 0.39;   // 衰减系数 先从0.25开始，后面可再调大
         const double tau_ff_limit_hip = 4.0;
         const double tau_ff_limit_thigh = 30.0;
-        const double tau_ff_limit_calf = 30.0;
+        const double tau_ff_limit_calf = 35.0;
 
         // 遍历4条腿，计算每条腿的关节力矩并赋值给控制接口
         for (int i = 0; i < 4; i++) {
