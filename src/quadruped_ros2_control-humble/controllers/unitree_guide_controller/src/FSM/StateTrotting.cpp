c711791962bd8094a057c4f3a56575b0455d5b84
sly3601 <1967584065@qq.com>
2026-05-01 16:57:05 +0800

    40

--- src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/FSM/StateTrotting.cpp
+++ src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/FSM/StateTrotting.cpp
@@ -37,12 +37,12 @@ StateTrotting::StateTrotting(CtrlInterfaces &ctrl_interfaces,
     // 提高摆动高度：避免足端落地过浅，增强整体支撑余量（适配1.5倍腿长）
     gait_height_ = 0.045;
     // 身体位置比例增益：大幅提高z轴抑制下沉，x/y提高增强平动控制（全局优化，无后腿单独补偿）
-    Kpp = Vec3(25.8, 25.8, 195.1).asDiagonal();
+    Kpp = Vec3(27.8, 27.8, 195.1).asDiagonal();
     // 身体速度阻尼增益：增强z轴阻尼抗抖动，x/y提高抑制大惯性超调
     Kdp = Vec3(5.5, 5.5, 5.6).asDiagonal();
     // 姿态比例增益：大幅提高（作用于roll/pitch/yaw），增强整体姿态稳定性，防止侧倒/前后趴
-    kp_pitch_ = 300;    // 1900
-    kp_roll_ = 192;    // 1900
+    kp_pitch_ = 460;    // 1900
+    kp_roll_ = 352;    // 1900
     kp_yaw_ = 0;     // 1900
         // 姿态角速度阻尼增益：重点提高roll/pitch对应轴（x/y），加快姿态收敛，避免倾斜加剧
     Kd_w_ = Vec3(4.1, 4.1, 4.1).asDiagonal();
@@ -626,8 +626,8 @@ void StateTrotting::calcTau() {
         // 足底反力没有精确控制，而是变成偏置力矩*小于1的比例系数进行修正控制
         const double tau_ff_scale = 0.65;   // 衰减系数 先从0.25开始，后面可再调大
         const double tau_ff_limit_hip = 4.0;
-        const double tau_ff_limit_thigh = 42.0;
-        const double tau_ff_limit_calf = 60.0;
+        const double tau_ff_limit_thigh = 62.0;
+        const double tau_ff_limit_calf = 80.0;
 
         // 遍历4条腿，计算每条腿的关节力矩并赋值给控制接口
         for (int i = 0; i < 4; i++) {
