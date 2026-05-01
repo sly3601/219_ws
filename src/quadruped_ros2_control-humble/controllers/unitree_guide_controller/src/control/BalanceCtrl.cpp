e27404c017cd5d289d2e276d32fbc2a06c689967
sly3601 <1967584065@qq.com>
2026-05-01 14:52:39 +0800

    34

--- src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/control/BalanceCtrl.cpp
+++ src/quadruped_ros2_control-humble/controllers/unitree_guide_controller/src/control/BalanceCtrl.cpp
@@ -20,7 +20,7 @@ BalanceCtrl::BalanceCtrl(const std::shared_ptr<QuadrupedRobot> &robot) {
     friction_mat_ << 1, 0, friction_ratio_, -1, 0, friction_ratio_, 0, 1, friction_ratio_, 0, -1,
             friction_ratio_, 0, 0, 1;
 
-    pcb_ = Vec3(-0.03, -0.01, 0.0);  //修改了质心位置，原本0 0 0 注意，这里的pcb_是质心位置，这个变量是balance控制器内部的private参数，和troting的pcb_没有关系了。
+    pcb_ = Vec3(0.0, 0.0, 0.0);  //修改了质心位置，原本0 0 0 注意，这里的pcb_是质心位置，这个变量是balance控制器内部的private参数，和troting的pcb_没有关系了。
     // pcb_ = Vec3(-0.10, -0.02, -0.04);
     // Ib_ = Vec3(0.0792, 0.2085, 0.2265).asDiagonal(); // 原来宇树的参数
     Ib_ = Vec3(0.624, 2.683, 2.934).asDiagonal(); // 暂定的参数
