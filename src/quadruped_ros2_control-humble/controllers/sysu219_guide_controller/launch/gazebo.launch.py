8df13e8684b4c16f6b13c3d30778bebdb3f3704d
sly3601 <1967584065@qq.com>
2026-06-10 01:29:34 +0800

    4

--- src/quadruped_ros2_control-humble/controllers/sysu219_guide_controller/launch/gazebo.launch.py
+++ src/quadruped_ros2_control-humble/controllers/sysu219_guide_controller/launch/gazebo.launch.py
@@ -1,9 +1,16 @@
 import os
 
 import xacro
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+    IncludeLaunchDescription,
+    RegisterEventHandler,
+    SetEnvironmentVariable,
+)
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
@@ -11,12 +18,23 @@ from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
+def prepend_env_value(name, value):
+    current_value = os.environ.get(name, "")
+    if not current_value:
+        return value
+    if value in current_value.split(os.pathsep):
+        return current_value
+    return value + os.pathsep + current_value
+
+
 def launch_setup(context, *args, **kwargs):
     package_description = context.launch_configurations['pkg_description']
     init_height = context.launch_configurations['height']
-    pkg_path = os.path.join(get_package_share_directory(package_description))
 
+    pkg_path = os.path.join(get_package_share_directory(package_description))
+    leg_pd_prefix = get_package_prefix('leg_pd_controller')
     xacro_file = os.path.join(pkg_path, 'xacro', 'robot.xacro')
+
     robot_description = xacro.process_file(
         xacro_file,
         mappings={
@@ -25,7 +43,11 @@ def launch_setup(context, *args, **kwargs):
         },
     ).toxml()
 
-    rviz_config_file = os.path.join(get_package_share_directory(package_description), "config", "visualize_urdf.rviz")
+    rviz_config_file = os.path.join(
+        get_package_share_directory(package_description),
+        "config",
+        "visualize_urdf.rviz"
+    )
 
     rviz = Node(
         package='rviz2',
@@ -57,7 +79,7 @@ def launch_setup(context, *args, **kwargs):
                 'use_sim_time': True,
                 'use_tf_static': True,
                 'robot_description': robot_description,
-                'ignore_timestamp': True
+                'ignore_timestamp': True,
             }
         ],
     )
@@ -65,58 +87,96 @@ def launch_setup(context, *args, **kwargs):
     joint_state_publisher = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster",
-                   "--controller-manager", "/controller_manager"],
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     imu_sensor_broadcaster = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["imu_sensor_broadcaster",
-                   "--controller-manager", "/controller_manager"],
+        arguments=[
+            "imu_sensor_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     leg_pd_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["leg_pd_controller",
-                   "--controller-manager", "/controller_manager"],
+        arguments=[
+            "leg_pd_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     sysu219_guide_controller = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["sysu219_guide_controller", "--controller-manager", "/controller_manager"],
+        arguments=[
+            "sysu219_guide_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
     )
 
     return [
+        SetEnvironmentVariable(
+            'AMENT_PREFIX_PATH',
+            prepend_env_value('AMENT_PREFIX_PATH', leg_pd_prefix),
+        ),
+
+        SetEnvironmentVariable(
+            'LD_LIBRARY_PATH',
+            prepend_env_value('LD_LIBRARY_PATH', os.path.join(leg_pd_prefix, 'lib')),
+        ),
+
         rviz,
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('gazebo_ros'),
-                                       'launch',
-                                       'gazebo.launch.py'])]),
-            launch_arguments={'verbose': 'true'}.items()),
+                [
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare('gazebo_ros'),
+                            'launch',
+                            'gazebo.launch.py',
+                        ]
+                    )
+                ]
+            ),
+            launch_arguments={'verbose': 'true'}.items(),
+        ),
+
         robot_state_publisher,
+
         gz_spawn_entity,
+
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=gz_spawn_entity,
                 on_exit=[joint_state_publisher],
             )
         ),
+
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=joint_state_publisher,
                 on_exit=[imu_sensor_broadcaster],
             )
         ),
+
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=imu_sensor_broadcaster,
                 on_exit=[leg_pd_controller],
             )
         ),
+
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action=leg_pd_controller,
@@ -130,17 +190,19 @@ def generate_launch_description():
     pkg_description = DeclareLaunchArgument(
         'pkg_description',
         default_value='sysu219_description',
-        description='package for robot description'
+        description='package for robot description',
     )
 
     height = DeclareLaunchArgument(
         'height',
         default_value='0.5',
-        description='Init height in simulation'
+        description='Init height in simulation',
     )
 
-    return LaunchDescription([
-        pkg_description,
-        height,
-        OpaqueFunction(function=launch_setup),
-    ])
+    return LaunchDescription(
+        [
+            pkg_description,
+            height,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
