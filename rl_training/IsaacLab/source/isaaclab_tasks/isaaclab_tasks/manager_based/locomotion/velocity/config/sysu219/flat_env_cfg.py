5b7f07f93acd9272902de15df186a8e019cd5976
sly3601 <1967584065@qq.com>
2026-07-03 23:54:42 +0800

    7

--- rl_training/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/sysu219/flat_env_cfg.py
+++ rl_training/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/sysu219/flat_env_cfg.py
@@ -7,6 +7,11 @@ from isaaclab.utils import configclass
 
 from .rough_env_cfg import SYSU219RoughEnvCfg
 
+# 新增
+from isaaclab.managers import RewardTermCfg as RewTerm
+from isaaclab.managers import SceneEntityCfg
+import isaaclab_tasks.manager_based.locomotion.velocity.mdp as mdp
+
 
 @configclass
 class SYSU219FlatEnvCfg(SYSU219RoughEnvCfg):
@@ -18,14 +23,52 @@ class SYSU219FlatEnvCfg(SYSU219RoughEnvCfg):
         self.rewards.flat_orientation_l2.weight = -2.5
         self.rewards.feet_air_time.weight = 0.25
 
-        # 降低第一阶段训练速度命令范围
+        # 原地踏步
         # self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.0)
         # self.commands.base_velocity.ranges.lin_vel_y = (0.0, 0.0)
         # self.commands.base_velocity.ranges.ang_vel_z = (0.0, 0.0)
 
-        self.commands.base_velocity.ranges.lin_vel_x = (-0.2, 0.3)
+        # 目标速度范围
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.5, 0.5)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.05, 0.05)
-        self.commands.base_velocity.ranges.ang_vel_z = (-0.2, 0.2)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.5, 0.5)
+
+        # 惩罚足端接触地面时的水平滑动
+        self.rewards.feet_slide = RewTerm(
+            func=mdp.feet_slide,
+            weight=-0.5,
+            params={
+                "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_foot"),
+                "asset_cfg": SceneEntityCfg("robot", body_names=".*_foot"),
+            },
+        )
+
+        # 电机扭矩过大惩罚
+        self.rewards.dof_torques_l2.weight = -0.0001
+
+        self.rewards.track_lin_vel_xy_exp.weight = 2.5  # xy 线速度跟踪奖励权重
+
+        self.rewards.lin_vel_z_l2.weight = -3.7          # 机身 z 方向速度惩罚
+        self.rewards.ang_vel_xy_l2.weight = -0.1         # 机身 roll/pitch 角速度惩罚
+        self.rewards.flat_orientation_l2.weight = -1.0   # 机身姿态倾斜惩罚；越负越希望身体保持水平
+
+        self.rewards.action_rate_l2.weight = -0.02  # 动作变化率惩罚
+        self.rewards.dof_acc_l2.weight = -5.0e-7    # 关节加速度惩罚
+
+        # 目标速度小于0.4时，要限制髋关节偏离默认角度。
+        self.rewards.stand_still_hip_deviation = RewTerm(
+            func=mdp.stand_still_joint_deviation_l1,
+            weight=-0.8,
+            params={
+                "command_name": "base_velocity",
+                "command_threshold": 1.0,  # 目标速度小于1.0时，才会惩罚髋关节偏离默认角度
+                "asset_cfg": SceneEntityCfg("robot", joint_names=".*_hip_joint"),
+            },
+)
+
+
+
+
 
         # change terrain to flat
         self.scene.terrain.terrain_type = "plane"
--- rl_training/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/sysu219/rough_env_cfg.py
+++ rl_training/IsaacLab/source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/sysu219/rough_env_cfg.py
@@ -52,7 +52,17 @@ class SYSU219RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         # rewards
         self.rewards.feet_air_time.params["sensor_cfg"].body_names = ".*_foot"
         self.rewards.feet_air_time.weight = 0.01
-        self.rewards.undesired_contacts = None
+
+        # 新增 增加对不希望接触的部位的惩罚
+        # self.rewards.undesired_contacts = None
+        self.rewards.undesired_contacts.params["sensor_cfg"].body_names = [
+            ".*_hip",
+            ".*_thigh",
+            ".*_calf",
+            ".*_rotor",
+        ]
+        self.rewards.undesired_contacts.weight = -1.0
+
         self.rewards.dof_torques_l2.weight = -0.0002
         self.rewards.track_lin_vel_xy_exp.weight = 1.5
         self.rewards.track_ang_vel_z_exp.weight = 0.75
